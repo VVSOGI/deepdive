2ab88c630ac0aa359c4b7b1c48be494a7506c215
benny <vvsogi@gmail.com>
2024-09-11 15:01:35 +0900

    Refactor chunk saving logic

--- playground/network/chunk/index.js
+++ playground/network/chunk/index.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const fs = require("fs");
-const save = require("./utils/save.js");
 
 const { outputDir, readmePath, chunkSize } = require("./config.js");
 
@@ -15,8 +14,21 @@ function init() {
       return;
     }
   });
-  const chunksFolder = path.resolve(__dirname, outputDir);
-  save(data, chunkSize, chunksFolder);
+
+  const chunks = [];
+  for (let i = 0; i < data.length; i += chunkSize) {
+    const encodedChunk = Buffer.from(data.slice(i, i + chunkSize)).toString("base64");
+    chunks.push(encodedChunk);
+  }
+
+  chunks.forEach((chunk, index) => {
+    fs.writeFile(path.resolve(__dirname, outputDir) + `/chunk-${index + 1}.txt`, chunk, (err) => {
+      if (err) {
+        console.error("Error occured when save file: ", err);
+        return;
+      }
+    });
+  });
 }
 
 init();

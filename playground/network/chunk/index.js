2c1c7b94fe5446ae71d0f94d3e5296a49f4746bb
benny <vvsogi@gmail.com>
2024-09-11 14:54:50 +0900

    Remove unused readFile function and update file reading in init function

--- playground/network/chunk/index.js
+++ playground/network/chunk/index.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const fs = require("fs");
 const save = require("./utils/save.js");
-const readFile = require("./utils/readfile.js");
 
 const { outputDir, readmePath, chunkSize } = require("./config.js");
 
@@ -9,8 +8,13 @@ if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir);
 }
 
-async function init() {
-  const data = await readFile(readmePath);
+function init() {
+  const data = fs.readFileSync(readmePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error occured when read file: ", err);
+      return;
+    }
+  });
   const chunksFolder = path.resolve(__dirname, outputDir);
   save(data, chunkSize, chunksFolder);
 }

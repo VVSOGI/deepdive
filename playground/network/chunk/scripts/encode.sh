f8cf8b91c2b0cc89c5eced5fc4bdec1b02939d09
benny <vvsogi@gmail.com>
2024-09-11 17:10:07 +0900

    Refactor encode.sh script to use absolute paths for target and output files

--- playground/network/chunk/scripts/encode.sh
+++ playground/network/chunk/scripts/encode.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 set -e
+to_absolute_path() {
+    local path="$1"
+    if [[ "$path" != /* ]]; then
+        path="$PWD/$path"
+    fi
+    echo "$(cd "$(dirname "$path")" && pwd)/$(basename "$path")"
+}
+
+TARGET_PATH=$(to_absolute_path "$1")
+OUTPUT_PATH=$(to_absolute_path "$2")
+
 cd "$(dirname "$0")"
 
-node index.js 
+node ../utils/encode.js -t "$TARGET_PATH" -o "$OUTPUT_PATH"

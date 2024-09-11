fb1e6db06f25696a7bc91062e488597f8bfee1ab
benny <vvsogi@gmail.com>
2024-09-11 18:09:19 +0900

    Remove unused variable in config.js

--- playground/network/chunk/config.js
+++ playground/network/chunk/config.js
@@ -1,10 +1,8 @@
-const readmePath = "README.md";
 const chunkSize = 500;
 const encodeFolder = "just-chunk-encoded";
 const decodeFolder = "just-chunk-decoded";
 
 module.exports = {
-  readmePath,
   chunkSize,
   encodeFolder,
   decodeFolder,

24463f151ce605dca50e892f1240e4d20e30854f
benny <vvsogi@gmail.com>
2024-10-01 17:24:57 +0900

    Refactor check function in src/check.ts

--- playground/typia-test/src/check.ts
+++ playground/typia-test/src/check.ts
@@ -1,17 +1,15 @@
 import typia, { tags } from "typia";
 
-export const check = typia.createIs<IMember>();
+export const check: (input: IMember) => input is IMember = typia.createIs<IMember>();
 
 interface IMember {
   id: string & tags.Format<"uuid">;
   email: string & tags.Format<"email">;
-  age: number & tags.ExclusiveMinimum<19> & tags.Maximum<100>;
+  age: number & tags.Type<"uint32"> & tags.ExclusiveMinimum<19> & tags.Maximum<100>;
 }
 
-console.log(
-  check({
-    id: "eef46580-5ea1-4ab3-a9be-329f6a793deb",
-    email: "test@tset.com",
-    age: 20,
-  })
-);
+check({
+  id: "eef46580-5ea1-4ab3-a9be-329f6a793deb",
+  email: "email@email.eam",
+  age: 20,
+});
